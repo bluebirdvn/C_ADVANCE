2e1bcbdef8c63b5e9c6e4b1ca7a48e4872f0734e
Blue Bird <shun.bluebird@gmail.com>
2025-05-27 23:33:02 +0700

    add new case to createSensor

--- factory_pattern/sensorFactory/sensor_factory.c
+++ factory_pattern/sensorFactory/sensor_factory.c
@@ -1,2 +1,33 @@
 #include "sensor_factory.h"
+#include "../humidity/humidity.h"
+#include "../light/light.h"
+#include "../temperature/temperature.h"
+#include "../pressure/pressure.h"
 
+
+Sensor* createSensor(SensorType type) {
+    Sensor* sensor = (Sensor*)malloc(sizeof(Sensor));
+    if (sensor == NULL) {
+        return NULL;
+    }
+
+    switch (type) {
+        case SENSOR_TEMPERATURE:
+            *sensor = createTemperatureSensor();
+            break;
+        case SENSOR_LIGHT:
+            *sensor = createLightSensor();
+            break;
+        case SENSOR_HUMIDITY:
+            *sensor = createHumiditySensor();
+            break;
+        case SENSOR_PRESSURE:
+            *sensor = createPressureSensor();
+            break;
+        default:
+            free(sensor);
+            return NULL;
+        
+    }
+    return sensor;
+}
