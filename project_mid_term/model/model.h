75086c6098be933224066bf55333312876b30602
Tuan <nguyenvantuan150203@gmail.com>
2025-08-09 00:53:25 +0700

    add system_init

--- project_mid_term/model/model.h
+++ project_mid_term/model/model.h
@@ -1,17 +1,23 @@
 #ifndef _MODEL_H
 #define _MODEL_H
 
-#include "cpu.h"
-#include "network.h"
-#include "storage.h"
-#include "system.h"
-#include "memory.h"
+#include "cpu/cpu.h"
+#include "network/network.h"
+#include "storage/storage.h"
+#include "system/system.h"
+#include "memory/memory.h"
 
-typedef struct {
+typedef struct System_monitor system_monitor;
+
+struct System_monitor{
     Storage_info *storage_info;
     Network_info *network_info;
     Memory_info *memory_info;
-    cpu_usage *cpu_usage;
-}
+    Cpu_usage *cpu_usage;
+
+
+} ;
+
+System_monitor *system_monitor_init(void);
 
 #endif //_MODEL_H
